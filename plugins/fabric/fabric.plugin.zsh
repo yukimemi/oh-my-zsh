a8b6abb46be53fbc5231899dbc17770a3ea3f184
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-06 14:08:12 +0800

    Add new plugin to autocomplete fabric commands

--- plugins/fabric/fabric.plugin.zsh
+++ plugins/fabric/fabric.plugin.zsh
@@ -1,1 +1,8 @@
-# DECLARION: This plugin was created by vhbit. What I did is just making a portal from https://github.com/vhbit/fabric-zsh-autocomplete.
+#compdef fab
+
+_fab_list() {
+    reply=(`fab --shortlist`)
+}
+compctl -K _fab_list fab
+
+# DECLARION: This plugin was created by kennethreitz. What I did is just making a portal from https://github.com/kennethreitz-archive/fabric-zsh-completion.
